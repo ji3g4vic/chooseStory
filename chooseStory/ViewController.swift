5837779c3a176f515d61ca3bbc24d0304da5d073
Vic <ji3g4vic03@gmail.com>
2023-04-04 16:58:57 +0800

    Initial Commit

---
+++ chooseStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chooseStory
+//
+//  Created by 王衍智 on 2023/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
